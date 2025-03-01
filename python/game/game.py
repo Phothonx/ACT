ba33083f7d38820b00d7fa78cc92afc679e2d304
Phothonx <86020708+Phothonx@users.noreply.github.com>
2025-03-01 17:15:32 +0100

    pls don't read the code

--- python/game/game.py
+++ python/game/game.py
@@ -7,26 +7,32 @@ class Game():
   def __init__(self)->None:
     self.round = -1
     self.turn = -1
-    # self.playing = -1
+    self.playing = -1
 
   def draw(self):
     grid.draw()
     for player in players:
       for pawn in player.pawns:
         pawn.draw(self.turn)
+    if self.playing != -1:
+      players[self.playing].pawns[-1].draw_mvts(self.turn)
 
   def update(self):
-    if self.round >= 3:
+    if self.round  >= 3 and self.playing <= -1:
       print("FIN")
       quit()
-    elif self.turn >= 5 or self.round <= -1:
+    elif (self.turn >= 2 or self.round <= -1) and self.playing <= -1:
       self.newRound()
+    elif self.playing <= -1 or self.turn <= -1:
+      self.newTurn()
     else:
-      first_player = players[self.round%2]
-      self.play(first_player)
-      second_player = players[(self.round+1)%2]
-      self.play(second_player)
-      self.turn += 1
+      self.play(players[self.playing])
+
+      # first_player = players[self.round%2]
+      # self.play(first_player)
+      # second_player = players[(self.round+1)%2]
+      # self.play(second_player)
+      # self.turn += 1
     # elif self.playing <= -1:
     #   self.newTurn()
     #   self.nextPlayer()
@@ -38,13 +44,15 @@ class Game():
     round = self.round
     if playing == -1: # si tout le monde a joué
       self.playing = round%2 # on alterne le premier à jouer 1 round /2
+      print("    - Joueur " + str(self.playing) + " joue")
     elif round%2 == (playing+1)%2: # si c'est le 2eme joueur du round qui a joué
       self.playing = -1 # il n'y a plus personne qui doit jouer
     else:
       self.playing = (playing+1)%2 # sinon c'est à l'autre de jouer
+      print("    - Joueur " + str(self.playing) + " joue")
 
   def play(self, player):
-    print("    - Joueur " + str(player.id) + " joue")
+    # print(self.turn)
     if len(player.pawns) == 0:
       raise ValueError("No pawns to be played")
     if player.pawns[-1].select_move(self.turn):
@@ -52,14 +60,20 @@ class Game():
 
   def newRound(self):
     self.round += 1
-    # if self.turn != -1 or self.turn != 5:
-    #   raise ValueError(f"Round ended on wrong turn ({self.turn})")
+    if self.turn != -1 and self.turn != 2:
+      raise ValueError(f"Round ended on wrong turn ({self.turn})")
+    if self.playing != -1:
+      raise ValueError(f"Bad end of round, ended on player{self.playing}'s turn")
     print("@ Round: " + str(self.round))
-    self.turn = 0 # come back to the past baby
+    self.turn = -1 # come back to the past baby
     for player in players:
-      player.pawns.append(Soldier((5, 5), player))
+      new_pawn = Soldier((5, 5), player)
+      player.pawns.append(new_pawn)
+      # print(player.pawns)
 
   def newTurn(self):
+    if self.playing != -1:
+      raise ValueError(f"Bad end of turn, ended on player{self.playing}'s turn")
     self.turn += 1
     print("  * Tour: " + str(self.turn))
     self.nextPlayer()
--- python/pawns/pawn.py
+++ python/pawns/pawn.py
@@ -15,29 +15,40 @@ class Pawn:
   SPRITE_P1 = (24, 0)
 
   def __init__(self, spawn, owner):
-    self.owner = owner # int player 0, 1
+    self.owner = owner
     spawn_state = Pawnstate(self.MAX_LIFE, spawn)
     self.state = [ spawn_state ] # state of the pawn for a certain turn in this array
-    self.sprite = self.SPRITE_P0 if owner == 0 else self.SPRITE_P1
+    self.sprite = self.SPRITE_P0 if owner.id == 0 else self.SPRITE_P1
     # self.pos = spawn # initial position
     # self.life = self.MAX_LIFE
 
   def move(self, turn, deplacement): # edit next pawn state
-    state = self.state[turn]
-    state.pos = addt(state.pos, deplacement)
-    self.state[turn + 1] = state
+    pos = self.state[turn].pos
+    npos = addt(pos, deplacement)
+    nlife = self.state[turn].life
+    new_state = Pawnstate(nlife, npos)
+    if len(self.state) < turn:
+      raise ValueError(f"Move: Missing pawn state {turn}")
+    self.state[turn] = new_state
+    if turn+1 >= len(self.state): # on ajoute un state si il n'existe pas encore
+      self.state.append(new_state)
+    else:
+      self.state[turn + 1] = new_state
 
   def draw(self, turn):
-    pyxel.blt(
-      self.state[turn].pos[0]*TILES_WIDTH,
-      self.state[turn].pos[1]*TILES_HEIGHT,
-      0,
-      self.sprite[0],
-      self.sprite[1],
-      TILES_WIDTH, # a pawn is the size of a tile
-      TILES_HEIGHT,
-      0 # black is colkey
-    )
+    if len(self.state) < turn:
+      raise ValueError(f"Draw: Missing pawn state {turn} for player {self.owner.id}")
+    if turn != -1:
+      pyxel.blt(
+        self.state[turn].pos[0]*TILES_WIDTH,
+        self.state[turn].pos[1]*TILES_HEIGHT,
+        0,
+        self.sprite[0],
+        self.sprite[1],
+        TILES_WIDTH, # a pawn is the size of a tile
+        TILES_HEIGHT,
+        0 # black is colkey
+      )
 
   def select_move(self, turn):
     cardinals = [ (0, 1), (1, 0), (0, -1), (-1, 0) ]
@@ -50,15 +61,16 @@ class Pawn:
 
   def draw_mvts(self, turn):
     cardinals = [ (0, 1), (1, 0), (0, -1), (-1, 0) ]
-    for direction in cardinals:
-      hlpos = addt(self.state[turn].pos, direction)
-      pyxel.blt(
-        hlpos[0]*TILES_WIDTH,
-        hlpos[1]*TILES_HEIGHT,
-        0,
-        32,
-        0,
-        TILES_WIDTH,
-        TILES_HEIGHT,
-        0
-      )
+    if turn != -1:
+      for direction in cardinals:
+        hlpos = addt(self.state[turn].pos, direction)
+        pyxel.blt(
+          hlpos[0]*TILES_WIDTH,
+          hlpos[1]*TILES_HEIGHT,
+          0,
+          32,
+          0,
+          TILES_WIDTH,
+          TILES_HEIGHT,
+          0
+        )
